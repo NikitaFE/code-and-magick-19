556a37da9c88a0a6b9ebe5ae00e46c295a2dace8
NikitaFE <nikromongerredd@gmail.com>
2020-02-24 20:40:56 +0200

    Вносит поправки в файл util.js

--- js/util.js
+++ js/util.js
@@ -4,11 +4,13 @@
   var ENTER_CODE = 13;
   var ESC_CODE = 27;
 
+  var getRandomItem = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   window.util = {
     ENTER_CODE: ENTER_CODE,
     ESC_CODE: ESC_CODE,
-    getRandomItem: function (arr) {
-      return arr[Math.floor(Math.random() * arr.length)];
-    }
+    getRandomItem: getRandomItem
   };
 })();
